905692c3a2325a72c6b67a667986e7b21754ccef
Stanley Kao <stanleykao72@gmail.com>
2020-02-15 23:12:12 +0800

    linebot QnA initial

--- linebotQnA/linebotQnA/urls.py
+++ linebotQnA/linebotQnA/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
-from qnaapi import views
+
 
 urlpatterns = [
     # url('^callback', views.callback),
