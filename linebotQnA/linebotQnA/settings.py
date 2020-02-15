1c680ef43e595fc0e9a90a60beaf46542a399440
Stanley Kao <stanleykao72@gmail.com>
2020-02-15 22:39:51 +0800

    linebot QnA initial

--- linebotQnA/linebotQnA/settings.py
+++ linebotQnA/linebotQnA/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tysvfbz5z-z$7q(1s(58sj$fhbhdgl*%d9v51$8v3o#v710o(e'
 # LINE_CHANNEL_ACCESS_TOKEN = '4z3/8qa8A64sRanN/637QpEnUxFc6SbNssKv1lhmHiOwumt5DV2YxK7NxbgxRECkHwUx2Ko1saQmB+Az8/IA3XjihKF59sUnzppdIKv4CDdskRUFmffSwLFXIyca5klH8aV+D9xGlJwF2xFOr12b4AdB04t89/1O/w1cDnyilFU='
 # LINE_CHANNEL_SECRET = '272fa90a0dd35c051bb05566f3cb3014'
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
 HOST = os.getenv("HOST")
 ENDPOINT_KEY = os.getenv("ENDPOINT_KEY")
