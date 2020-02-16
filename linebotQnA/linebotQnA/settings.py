d6121c9900d7b111222355c0fb12684d69a55bff
Stanley Kao <stanleykao72@gmail.com>
2020-02-16 15:28:04 +0800

    linebot QnA initial

--- linebotQnA/linebotQnA/settings.py
+++ linebotQnA/linebotQnA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,6 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('zh-hans', _('Simplified Chinese')),
+    ('zh-hant', _('Traditional Chinese')),
+)
 
 LANGUAGE_CODE = 'zh-Hant'
 
